f5538835b594d69d81c07cb0f075d76ae0e1c874
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 03:44:56 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,57 @@
 # Write your methods here
+
+def loop_message_five_times(msg_string)
+count = 0
+while count <= 5 do
+  puts msg_string
+  count +=1
+end
+end
+
+def loop_message_n_times(msg_string, n)
+count = 0
+while count < n do 
+  puts msg_string
+  count += 1
+end
+end
+
+def output_array(array)
+  count = 0
+  arrLen = array.size
+    while (count < arrLen)  do 
+      puts array[count]
+      count += 1
+    end
+end
+
+def return_string_array(array)
+  count = 0
+  arrLen = array.length
+  while (count < arrLen) do 
+    array[count] = array[count].to_s
+    count += 1
+  end
+  return array
+end
+
+return_string_array([2, "string1", "2", 13, 12.5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
